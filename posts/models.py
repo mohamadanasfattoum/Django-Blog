c88b5654909ee4076846db82a1f907315f969c50
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-19 00:05:18 +0200

    add true code --DateTimeField--

--- posts/models.py
+++ posts/models.py
@@ -6,4 +6,4 @@ from django.db import models
 class Post (models.Model):
     title= models.CharField(max_length=120)
     comment = models.TextField(max_length=5000)
-    publish_date = models.TimeField()
+    publish_date = models.DateTimeField()
