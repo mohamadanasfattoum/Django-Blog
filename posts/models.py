5075bce4b506afcaead2096f3537b5252f6885d5
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-18 23:43:16 +0200

    add class post in models(data base)

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post (models.Model):
+    title= models.CharField(max_length=120)
+    comment = models.TextField(max_length=5000)
+    publish_date = models.TimeField()
