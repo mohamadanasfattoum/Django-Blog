6516740096e59c3b7c54f13f92fd7c331c109df9
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-19 00:29:38 +0200

    add auther in models , create User

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -7,3 +7,8 @@ class Post (models.Model):
     title= models.CharField(max_length=120)
     comment = models.TextField(max_length=5000)
     publish_date = models.DateTimeField()
+    auther = models.ForeignKey(User,related_name='post_auther',on_delete=models.SET_NULL,null=True)
+
+
+    def __str__(self) -> str:
+        return self.title
