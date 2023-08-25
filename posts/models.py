779c29f6d99bea64aa7d50edec30d27cd618480a
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-25 20:20:19 +0200

    add creat_date in  models.py

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
-
+from django.utils import timezone
 
 # Create your models here.
 
@@ -22,4 +22,6 @@ class Post (models.Model):
 
 class Comment (models.Model):
     auther = models.ForeignKey(User, related_name= 'comment_auther', on_delete=models.CASCADE)
+    
     comment = models.TextField(max_length=500)
+    creat_date = models.DateTimeField( default= timezone.now)
