35b02e582884b511097f90d8b89c03c202b269f3
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-25 21:01:52 +0200

    correction def --str-- and add comment in test 1

--- posts/admin.py
+++ posts/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Post
-from .models import Comment
+from .models import Post , Comment
 
 
 admin.site.register(Post)
--- posts/models.py
+++ posts/models.py
@@ -27,5 +27,5 @@ class Comment (models.Model):
     creat_date = models.DateTimeField( default= timezone.now)
 
 
-    def __str__(self) -> str:
-        return str(self.title)
+    def __str__(self):
+        return str(self.post)
