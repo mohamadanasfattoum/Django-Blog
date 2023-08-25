8009c09db64b9d774bfa6625131eab0d1a80f331
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-25 20:11:07 +0200

    correction auther in  models.py

--- posts/models.py
+++ posts/models.py
@@ -21,5 +21,5 @@ class Post (models.Model):
 
 
 class Comment (models.Model):
-    auther = models.ForeignKey(Post, related_name= 'comment_auther', on_delete=models.CASCADE)
+    auther = models.ForeignKey(User, related_name= 'comment_auther', on_delete=models.CASCADE)
     comment = models.TextField(max_length=500)
