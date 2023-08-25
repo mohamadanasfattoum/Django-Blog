c7eb0673471d5958bf4a50dc48b21ce9a4d2ab1f
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-25 20:44:15 +0200

    add Comment in admin

--- posts/admin.py
+++ posts/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Post
+from .models import Comment
 
 
 admin.site.register(Post)
+admin.site.register(Comment)
--- posts/models.py
+++ posts/models.py
@@ -10,7 +10,7 @@ class Post (models.Model):
     auther = models.ForeignKey(User,related_name='post_auther',on_delete=models.SET_NULL,null=True)
     title= models.CharField(max_length=120)
     comment = models.TextField(max_length=5000)
-    publish_date = models.DateTimeField()
+    publish_date = models.DateTimeField(default= timezone.now)
     tags = TaggableManager()
     image = models.ImageField(upload_to='posts', null=True) 
 
