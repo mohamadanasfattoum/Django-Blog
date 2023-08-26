8357ce9f4dc57c5542aa94dbd7fc87f3fea7c6e5
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-26 14:18:47 +0200

    add post_list : in urls.py

--- project/urls.py
+++ project/urls.py
@@ -18,12 +18,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from posts.views import post_list
 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/',post_list),
+    
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
