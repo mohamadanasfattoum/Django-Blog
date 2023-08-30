9b69f4d34f2307f7c6f4d9e8e9214c32bc8b120d
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-30 21:46:54 +0200

    add edit_post in urls

--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from posts.views import post_list, post_detail, add_post
+from posts.views import post_list, post_detail, add_post, edit_post
 
 
 
@@ -28,6 +28,7 @@ urlpatterns = [
     path('blog/',post_list),
     path('blog/add/',add_post),
     path('blog/<int:post_id>/',post_detail),
+    path('blog/<int:post_id>/edit/',edit_post),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
