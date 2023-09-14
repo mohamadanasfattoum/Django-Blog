93044396c5d195a0f7783bb43e5982d9c6e0bb8b
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-14 21:56:23 +0200

    add class postlist in urls

--- project/urls.py
+++ project/urls.py
@@ -19,13 +19,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from posts.views import post_list, post_detail, add_post, edit_post , delete_post
-
+from posts.views2 import PostList
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',post_list),
+    path('blog/',PostList.as_view()),
     path('blog/add/',add_post),
     path('blog/<int:post_id>/',post_detail),
     path('blog/<int:post_id>/edit/',edit_post),
